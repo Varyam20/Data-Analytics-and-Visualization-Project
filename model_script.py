085703866939d84290837d14ff21a5ca03cc47da
Varyam20 <varyamkaurrai.bt23cseds@pec.edu.in>
2025-06-30 00:05:34 +0530

    Update model_script.py

--- model_script.py
+++ model_script.py
@@ -4,12 +4,12 @@ import json
 import numpy as np
 
 # Load label mapping
-with open(r"D:\DAV Project\label_mapping.json") as f:
+with open("label_mapping.json") as f:
     label_mapping = json.load(f)
     label_reverse = {v: k for k, v in label_mapping.items()}
 
 # Load the trained pipeline
-loaded_model = joblib.load(r"D:\DAV Project\crop_recommender_rf.pkl")
+loaded_model = joblib.load("crop_recommender_rf.pkl")
 
 # Predict function
 def predict_crop(N, P, K, temperature, humidity, pH, rainfall):
